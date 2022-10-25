e3fb1b2ba5e241b46f467e2d139e5ecc5203d5cb
cmstiver <chayse.stiver@gmail.com>
2022-10-25 14:07:16 -0700

    getting somewhere

--- src/main.jsx
+++ src/main.jsx
@@ -15,40 +15,43 @@ import EditContact, { action as editAction } from "./routes/edit";
 import { action as destroyAction } from "./routes/destroy";
 import Index from "./routes/index";
 
-const router = createBrowserRouter([
-    {
-        path: "/",
-        element: <Root />,
-        errorElement: <ErrorPage />,
-        loader: rootLoader,
-        action: rootAction,
-        children: [
-            {
-                errorElement: <ErrorPage />,
-                children: [
-                    { index: true, element: <Index /> },
-                    {
-                        path: "contacts/:contactId",
-                        element: <Contact />,
-                        loader: contactLoader,
-                        action: contactAction,
-                    },
-                    {
-                        path: "contacts/:contactId/edit",
-                        element: <EditContact />,
-                        loader: contactLoader,
-                        action: editAction,
-                    },
-                    {
-                        path: "contacts/:contactId/destroy",
-                        action: destroyAction,
-                        errorElement: <div>Oops! There was an error.</div>,
-                    },
-                ],
-            },
-        ],
-    },
-]);
+const router = createBrowserRouter(
+    [
+        {
+            path: "/",
+            element: <Root />,
+            errorElement: <ErrorPage />,
+            loader: rootLoader,
+            action: rootAction,
+            children: [
+                {
+                    errorElement: <ErrorPage />,
+                    children: [
+                        { index: true, element: <Index /> },
+                        {
+                            path: "contacts/:contactId",
+                            element: <Contact />,
+                            loader: contactLoader,
+                            action: contactAction,
+                        },
+                        {
+                            path: "contacts/:contactId/edit",
+                            element: <EditContact />,
+                            loader: contactLoader,
+                            action: editAction,
+                        },
+                        {
+                            path: "contacts/:contactId/destroy",
+                            action: destroyAction,
+                            errorElement: <div>Oops! There was an error.</div>,
+                        },
+                    ],
+                },
+            ],
+        },
+    ],
+    { basename: "/react-router" }
+);
 
 ReactDOM.createRoot(document.getElementById("root")).render(
     <React.StrictMode>
